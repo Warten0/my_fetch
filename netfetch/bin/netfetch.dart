ede85dd73fefe39ef7914fb0348809100c9571d1
Warten0 Warten0 <1524763508@qq.com>
2023-05-29 03:25:33 +0000

    SAVE

--- netfetch/bin/netfetch.dart
+++ netfetch/bin/netfetch.dart
@@ -2,7 +2,7 @@ import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) async {
   final httpPackageUrl = Uri.https('raw.githubusercontent.com',
-      '/YN3011140075/my_netfetch/main/my_data.json');
+      '/Warten0/my_fetch/main/my_data.json');
   final httpPackageInfo = await http.read(httpPackageUrl);
   print(httpPackageInfo);
 }
